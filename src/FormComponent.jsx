0639710f69b6a10272a1f4ddac3b712035178f48
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-05-25 20:34:39 +0300

    group 6

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 function FormComponent(){
     const[userName, setUsername]=useState("")
     const[password, setPassword]=useState("")
-    const[button, setButton]=useState("")
+    const[isValid, setIsValid]=useState(false)
+    const handleChangeUsername = (event) => {
+        // 👇 Get input value from "event"
+        setUsername(event.target.value);
+      };
+    const handleChangePassword = (event) => {
+        // 👇 Get input value from "event"
+        setPassword(event.target.value);
+      };
+    const checkValidity=()=>{
+        if (userName && password !=="" ){
+           setIsValid(true)
+        }else(
+           setIsValid(false)
+        )
+    }
+      useEffect(()=> {
+        checkValidity()
+        console.log(isValid)
+    },[userName, password])
+    function handleSubmit(uName,pwd){
+        // event.preventDefault()
+        console.log(uName, pwd)
+    } 
     return(
         <>
-        <form>
-           <input type="text" id='username-input' placeholder='Username'/>
-           <input type="text" id='password-input' placeholder='Password' />
-           <button id='login-button'>Submit</button>
+        <form >
+           <input onChange={handleChangeUsername}  type="text" name="username" id='username-input' placeholder='Username'/>
+           <input onChange={handleChangePassword} type="password" name="password" id='password-input' placeholder='Password' />
+           <button  onSubmit= {handleSubmit({userName, password})} disabled={!isValid} id='login-button' type="submit">Submit</button>
         </form>
         </>
     );
